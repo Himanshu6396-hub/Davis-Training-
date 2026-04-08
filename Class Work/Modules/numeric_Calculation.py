fd35562e4ac3380ac9c58d81789e45eb5f525bec
Himanshu <himanshu639632@gmail.com>
2026-04-08 10:22:47 +0530

    Add basic arithmetic functions in numeric_Calculation.py

---
+++ Class Work/Modules/numeric_Calculation.py
@@ -0,0 +1,15 @@
+# Function to calculate addition 
+def addition(a,b):
+    return (a+b)
+# Function to calculate subtraction 
+def subtraction(a,b):
+    return (a-b)
+#Function to calculate product 
+def product (a,b):
+    return (a*b)
+# Function to calculate division 
+def division(a,b):
+    return (a/b)
+# Function to calculate modules 
+def modules(a,b):
+    return (a%b)
