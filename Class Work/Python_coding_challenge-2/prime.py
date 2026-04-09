ae3c4d922b19229545dc84266d4b674c58e49684
Himanshu <himanshu639632@gmail.com>
2026-04-09 11:25:46 +0530

    Create prime.py

---
+++ Class Work/Python_coding_challenge-2/prime.py
@@ -0,0 +1,14 @@
+def is_prime(n):
+    if n < 2:
+        return "Not Prime"
+    
+    for i in range(2, n):
+        if n % i == 0:
+            return "Not Prime"
+    
+    return "Prime"
+
+# input
+n = int(input("Enter number: "))
+
+print(is_prime(n))
