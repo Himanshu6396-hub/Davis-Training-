cf16be62c610c6aaea5a48faadfce27d20d8a8c3
Himanshu <himanshu639632@gmail.com>
2026-04-09 09:42:09 +0530

    Create login_Validation.py

---
+++ Class Work/Python_coding_challenge-2/login_Validation.py
@@ -0,0 +1,10 @@
+# username and password 
+user= "student"
+password="Stu@123"
+#taking input to verify user and password 
+name=input("Enter user name:")
+key=input("Enter password")
+if user==name and password==key:
+  print("Login successfully")
+else:
+  print("Invalid credentials")
