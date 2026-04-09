6376564cc36613ceeff56430e3d9a004f435fec4
Himanshu <himanshu639632@gmail.com>
2026-04-09 10:07:38 +0530

    Create count_greater_number.py

---
+++ Class Work/Python_coding_challenge-2/count_greater_number.py
@@ -0,0 +1,11 @@
+# Input list
+numbers = [10, 60, 30, 80]
+
+# To count number greater than 50
+count = 0
+for num in numbers:
+    if num > 50:
+        count += 1
+
+# Output result
+print("Count =", count)
