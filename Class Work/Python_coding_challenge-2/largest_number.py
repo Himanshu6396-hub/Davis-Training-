8e81a9e69aefcbe15d4f34b51de3ae1064620bd3
Himanshu <himanshu639632@gmail.com>
2026-04-09 09:43:23 +0530

    Create largest_number.py

---
+++ Class Work/Python_coding_challenge-2/largest_number.py
@@ -0,0 +1,15 @@
+# Input three numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+# Find largest
+if a >= b and a >= c:
+    largest = a
+elif b >= a and b >= c:
+    largest = b
+else:
+    largest = c
+
+# Output result
+print("Largest =", largest)
