dbdd72bd3f62aad4147fa7c43f080e72b6f2549a
Himanshu <himanshu639632@gmail.com>
2026-04-09 09:50:51 +0530

    Create exam_grade.py

---
+++ Class Work/Python_coding_challenge-2/exam_grade.py
@@ -0,0 +1,16 @@
+# Input marks
+marks = int(input("Enter marks: "))
+
+# Assign grade
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
+# Output result
