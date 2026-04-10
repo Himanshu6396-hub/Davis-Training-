a2cbc886eab3c96f786a0c1d25ccb7c2ada52841
Himanshu <himanshu639632@gmail.com>
2026-04-10 10:42:03 +0530

    Update write_in_nextLine.py

--- Class Work/write_in_nextLine.py
+++ Class Work/write_in_nextLine.py
@@ -1,1 +1,23 @@
 
+# opening file in write mode
+filev = open("first.txt", "w")
+
+sentences = []
+
+print("Write any five sentences")
+for i in range(5):
+    sentence = input()
+    sentences.append(sentence + "\n")   # newline add kiya
+    print("___________")
+
+# writing multiple sentences
+filev.writelines(sentences)
+print("Data successfully written")
+
+filev.close()
+
+# reading file
+file = open("first.txt", "r")
+a = file.read()
+print(a)
+file.close()
