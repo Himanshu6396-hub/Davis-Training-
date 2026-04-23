728d7d886dfe9cc03fc3015cf41e8a51d993ec09
Himanshu <himanshu639632@gmail.com>
2026-04-23 10:43:40 +0530

    Create array_add_sub.py

---
+++ Class Work/Numpy/array_add_sub.py
@@ -0,0 +1,20 @@
+import numpy as np
+# first array
+a=np.array([10,12,48,73])
+# second array 
+b=np.array([46,86,94,55])
+print("--------------------")
+print("First array")
+print(a)
+print("Second array")
+print(b)
+print("------------------")
+# addition of array
+c=np.add(a,b)
+print("sum of two array")
+print(c)
+print("-----------")
+#subtraction of array 
+d=np.subtract(a,b)
+print(" Subtraction of two array")
+print(d)
